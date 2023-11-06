8877bea02b0682e2e4f526cdb2d9316682e56d76
Aaron Levy <aaronlevypc21@gmail.com>
2023-11-06 14:50:03 -0500

    codes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+  
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("bathe my dog"))
