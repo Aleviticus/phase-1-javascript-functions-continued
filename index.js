0a91d6b3c2122c4fe7ac524faaf03c6b87cb625f
Aaron Levy <aaronlevypc21@gmail.com>
2023-11-06 23:00:46 -0500

    updated code

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
-}
-  
+  }
   console.log(saturdayFun()); 
-  console.log(saturdayFun("bathe my dog"))
+  console.log(saturdayFun("bathe my dog")
+  );
+  
+function mondayWork(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home")
+);
+
+function wrapAdjective() {
+  return
+}
